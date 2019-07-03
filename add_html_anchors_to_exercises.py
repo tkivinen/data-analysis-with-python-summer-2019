261ac0ec17c1bb79419c0bc84a7011f725477af4
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2019-07-03 17:05:05 +0300

    attempt to add Univeristy of helsinki link and logo to pages

--- add_html_anchors_to_exercises.py
+++ add_html_anchors_to_exercises.py
@@ -9,6 +9,10 @@ def process(filename):
     with open(filename) as f:
         soup = BeautifulSoup(f, "lxml")
 
+    div = soup.find("div", class_="wy-side-nav-search")
+    a = BeautifulSoup("""<a href="http://www.helsinki.fi" style="margin-bottom: 0px;"><img src="https://uni.materialbank.net/NiboWEB/uni/getPublicFile.do?uuid=146263&amp;inline=false&amp;ticket=8a2a112700dc87abd2813d55e149bc0c&amp;type=original" style="margin-bottom: 0px;max-width: 60%;height: auto;width: auto;"></a>""", "html.parser")
+    div.insert(0, a)
+
     divs = soup.find_all("div", class_="admonition")
 
     for d in divs:
@@ -24,7 +28,8 @@ def process(filename):
             #d.string = '<a name="%s">%s</a>' % (exercise.replace(" ", "-"), exercise)
             #print("\n", d)
 
-    with open(filename, "w") as f:        
+
+    with open(filename, "w") as f:
         f.write(str(soup))
 
 for filename in sys.argv[1:]:
